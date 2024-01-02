4217b12b6769c2604f31deed94da1533cfb8bfc2
= <=>
2024-01-01 21:07:13 -0300

    minor changes

--- Main/main.cpp
+++ Main/main.cpp
@@ -3,9 +3,9 @@
 #include "cpp-httplib/httplib.h"
 
 void handle_request(const httplib::Request &req, httplib::Response &res) {
-  // Handle the request here
-  std::cout << req << std::endl;
+  // Handle the request here and print request to terminal
   res.set_content("Hello, World!", "text/plain");
+  std::cout << req.method << " " << req.path << std::endl;
 }
 
 int main() {
