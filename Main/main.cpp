2ee311062ee205289c313eb893d7b172f0507808
= <=>
2024-02-03 20:08:17 -0300

    Please enter the commit message for your changes. Lines starting

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,20 +1,3 @@
-#include "main.h"
-#include "cpp-httplib/httplib.h"
+#include <main.h>
 
-void handle_request(const httplib::Request &req, httplib::Response &res) {
-  // Handle the request here and print request to terminal
-  res.set_content("Hello, World!", "text/plain");
-  std::cout << req.method << " " << req.path << std::endl;
-}
 
-int main() {
-  httplib::Server server;
-
-  // Set the request handler
-  server.Get("/", handle_request);
-
-  // Start the server on port 8080
-  server.listen("localhost", 8080);
-
-  return 0;
-}
