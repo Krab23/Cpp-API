a118030e963aa9fcbc92bb187a00b09fcc875e68
= <=>
2024-02-03 23:51:24 -0300

    back to Hello World

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-#include <main.h>
+#include "main.h"
+#include <iostream>
 
-
+int main() {
+    struct Coordinate {
+      int x, y, z;
+    };
+    struct Coordinate origin = {0, 0, 0};
+    struct Coordinate *origin_pointer = &origin;
+    std::cout << origin.x << std::endl;
+    return 0;
+ }
